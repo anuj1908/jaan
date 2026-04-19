0bca1fe2f9c74dc0e8782a679fbf977a5c1516c4
Anuj Deshmukh <anujdeshmukh1908@gmail.com>
2026-04-19 14:52:22 +0530

    added files

---
+++ src/data/story.js
@@ -0,0 +1,28 @@
+import img1 from "../assets/images/img1.jpg";
+import img2 from "../assets/images/img2.jpg";
+import img3 from "../assets/images/img3.jpg";
+import img4 from "../assets/images/img4.jpg";
+import img5 from "../assets/images/img5.png";
+
+export const story = [
+  {
+    image: img1,
+    text: "Hiiii Baykoo, Just wanted to tell you somethingggg...",
+  },
+  {
+    image: img2,
+    text: "Do you remember this picture? Our first picture together ✨",
+  },
+  {
+    image: img3,
+    text: "Our first trip at Iskon... 🥺",
+  },
+  {
+    image: img4,
+    text: "A day full of enjoyymenttt 😌",
+  },
+  {
+    image: img5,
+    text: "Sometimes I do things that make you angry a lot, and I am really sorry for that.I know i lag at communication like hell yaar. But just like this image, be happy nd forget that anger plz . Plz maaf kar de jaan ❤️",
+  },
+];
